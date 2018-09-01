a62fd7640142aadde16d2b9daf0f6a5df8f0b1aa
Miguel Alfaiate <development@alfasoft.pt>
2018-09-01 21:01:10 +0100

    gitignore and screen corrections

--- sendControll.js
+++ sendControll.js
@@ -3,14 +3,19 @@ var flag = 2;
 var defaultHost = "localhost";
 var defaultPort = "8180";
 
+var GREEN = [124, 252, 0, 255];
+var RED = [255, 0, 0, 255];
+var YELLOW = [255, 205, 0, 255];
+
+var connectionStatus = false;
+
 if (typeof(Storage) == "undefined"){
 	localStorage.proxyConfig = "system";
 }
 
 var pconfig = localStorage.getItem("proxyConfig");
 
-if (pconfig == 'system') {
-		
+if (pconfig == 'system') { // disconnected
 	var proxySettings = {
 		proxyType: "system"
 	};
@@ -27,7 +32,7 @@ else if(pconfig == 'manual') {
 	document.getElementById("fixed_servers").setAttribute("hidden", "hidden");
 	document.getElementById("system").removeAttribute("hidden")
 	document.getElementById("settingsConfig").setAttribute("hidden", "hidden");
-
+	
 }
 
 //$('input[id=proxyTypeSystem]').click(function() {
@@ -45,9 +50,12 @@ document.getElementById("proxyTypeSystem").addEventListener('click', function() 
 	localStorage.proxyConfig = "system";
 	document.getElementById('proxyHostHttp').value = defaultHost;
     document.getElementById('proxyPortHttp').value = defaultPort;
+	
+	connectionStatus = false;
+	updateBadge(connectionStatus);
 });
 
-document.getElementById("proxyTypeManual").addEventListener('click', function() {
+document.getElementById("proxyTypeManual").addEventListener('click', function() { // connected
 	document.getElementById("fixed_servers").setAttribute("hidden", "hidden");
 	document.getElementById("system").removeAttribute("hidden")
 	document.getElementById("settingsConfig").setAttribute("hidden", "hidden");
@@ -56,7 +64,7 @@ document.getElementById("proxyTypeManual").addEventListener('click', function() 
 	var port = document.getElementById('proxyPortHttp').value
 	var local = ""
 	if(host.length > 0 && port.length >0){
-		local = host+":"+port
+		local = host + ":" + port
 	}
 	let proxySettings = {
 	  proxyType: "manual",
@@ -67,6 +75,9 @@ document.getElementById("proxyTypeManual").addEventListener('click', function() 
 	};
 	browser.proxy.settings.set({value: proxySettings})
 	localStorage.proxyConfig = "manual";
+	
+	connectionStatus = true;
+	updateBadge(connectionStatus);
 });
 
 
@@ -126,8 +137,11 @@ function onlineStatusResponseCheck(request) {
 		var response = JSON.parse(request.response);
 		
 		setServerIP(response.ip);
+		
+		updateBadge(true);
 	}
 	else if (request.status == 0) {
+		updateBadge(false);
 	}
 }
 
@@ -153,6 +167,83 @@ function checkOnlineStatus() {
 	xmlhttp.send();
 }
 
+
+
+
+
+
+
+// update connection stats if we are indeed connected
+function updateProxyStats() {
+	var haproxyIp = document.getElementById('proxyHostHttp').value;
+    var haproxyPort = parseInt(document.getElementById('proxyPortHttp').value, 10);
+	
+	// return and check later if host or port is invalid
+	if (haproxyIp == "" || isNaN(haproxyPort)) {
+		setTimeout(function() {
+			updateProxyStats();
+		}, 5000);
+		
+		return;
+	}
+	
+	var url = "http://" + haproxyIp + ":" + haproxyPort + "/haproxy_stats;csv";
+	
+	console.log("Checking HAProxy @ " + url);
+		
+	var xmlhttp = new XMLHttpRequest();
+	
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var haproxyStats = csvToArray(xmlhttp.responseText);
+			haproxyStats = JSON.stringify(haproxyStats[1]);
+			haproxyStats = haproxyStats.split(',');
+			haproxyStats[8] = haproxyStats[8].replace('"', '');
+			haproxyStats[9] = haproxyStats[9].replace('"', '');
+			console.log("Download: " + formatBytes(parseInt(haproxyStats[8])) + " / Upload: "+ formatBytes(parseInt(haproxyStats[9])));
+			
+			/*
+			setTimeout(function() {
+				updateProxyStats();
+			}, 5000);
+			*/
+		}
+		/*
+		console.log(xmlhttp);
+		console.log(xmlhttp.responseText);
+		*/
+	}
+
+	xmlhttp.open("GET", url, true);
+	xmlhttp.timeout = 2000; // time in milliseconds
+    xmlhttp.setRequestHeader('Access-Control-Allow-Origin','*');
+    xmlhttp.setRequestHeader('Access-Control-Allow-Methods', '*');
+    xmlhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+    xmlhttp.send();
+	
+	//setConnectionValues("Provider", "Service", "Time Online", "Server IP", "Data");
+}
+
+
+// update badge depending on connection status
+function updateBadge(connected) {
+	// if we don't check connection status extension will report connected with clients default ip address
+	if (connected == false || connectionStatus == false) {
+		browser.browserAction.setBadgeText({ text: "X" });
+		browser.browserAction.setBadgeBackgroundColor({color: RED});
+	}
+	else {
+		browser.browserAction.setBadgeText({ text: "O" });
+		browser.browserAction.setBadgeBackgroundColor({color: GREEN});
+	}
+}
+
+
+
+
+
 checkOnlineStatus();
+
+updateProxyStats();
 
 //document.getElementById("proxyTypeManual").addEventListener('click', getHaproxyStats());
